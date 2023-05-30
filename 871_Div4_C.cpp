483a2ac33cb8598aa1548b90b41eacc4231683f0
Asiful Haque <73637081+Asiful-Haque@users.noreply.github.com>
2023-05-30 13:20:46 +0600

    Create 871_Div4_C.cpp

---
+++ 871_Div4_C.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define check cout<<"hello"<<endl
+#define FASTERIO                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define EPS 1e-9
+#define PI acos(-1)
+#define distance(x1,x2,y1,y2) (((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)))
+#define printvector for(ll i=0;i<v.size();i++)cout<<v[i]<<" ";cout<<endl;
+#define printmap for(auto it=m.begin();it!=m.end();it++)cout<<it->first<<"-->"<<it->second<<endl;
+#define MAX (ll)(2e5+7)
+#define arrayzero memset(arr,0,sizeof(arr));
+#define s1_is_in_x x.find(s1)!=string::npos
+#define mod 1000000007
+//ll arr[MAX];
+// void sieve(){
+//     arr[2]=1;
+//     for(ll i=3;i<=MAX;i+=2){
+//         arr[i]=1;
+//     }
+//     for(ll i=3;i<=MAX;i+=2){
+//         for(ll j=i*i;j<=MAX;j+=i){
+//             arr[j]=0;
+//         }
+//     }
+// }
+int main(){
+	ll t;cin>>t;
+	while(t--){
+		ll n;cin>>n;
+		ll x;string s;
+		ll zero_one=INT_MAX,one_zero=INT_MAX,one_one=INT_MAX;
+		for(ll i=0;i<n;i++){
+			cin>>x;
+			cin>>s;
+
+			if(s=="01")zero_one=min(x,zero_one);
+			else if(s=="10")one_zero=min(x,one_zero);
+			else if(s=="11")one_one=min(x,one_one);
+		}
+		
+		if(one_zero!=INT_MAX && zero_one!=INT_MAX){
+			cout<<min((one_zero+zero_one),one_one)<<endl;
+		}
+		else if(one_zero==INT_MAX || zero_one==INT_MAX){
+			if(one_one!=INT_MAX){
+				cout<<one_one<<endl;
+			}
+			else{
+				cout<<"-1"<<endl;
+			}
+		}
+
+	}
+}
